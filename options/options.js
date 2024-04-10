87f7b1984dc1b0bc6c4b7c27bf856246043e4584
Crowdbotics <team@crowdbotics.com>
2024-04-10 23:28:10 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-thunder-47459",
+  url: "https://holy-thunder-47459.botics.co",
+  api: "https://holy-thunder-47459.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
